4c5bb885f351be2f6001d7c6aa6f7dd482a7ba0d
islameoa <91619542+islameoa@users.noreply.github.com>
2022-05-05 16:27:57 +0200

    ejercicio1 hecho

--- a01.js
+++ a01.js
@@ -1,12 +1,13 @@
  // Get the modal
  var modal = document.getElementById("myModal");
 
- // Get the image and insert it inside the modal - use its "alt" text as a caption
+ // agafem la imatge i el seu text alternatiu pero mostrar-los quan es cliqui al modal
  var img = document.getElementsByClassName("myImg");
  var modalImg = document.getElementById("img01");
  var captionText = document.getElementById("caption");
 
- // Get the image and insert it inside the modal - use its "alt" text as a caption
+ // Hem fet un getElementsByClassName perque per Id només agafa un element i com tenim 20 fotos necessitem un array d'elements que es recorrin
+ // amb un for, llavors cada cop que cliquem a una foto s'executa el seguent codi
 
  for (let index = 0; index < img.length; index++) {
      img[index].onclick = function() {
